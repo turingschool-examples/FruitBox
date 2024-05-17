3c3189c0fed96cb27f33d83918ddcc6139597131
nikss <nik.safe@gmail.com>
2024-05-17 08:46:41 -0700

    initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <nav></nav>
+      <h1>Fruit List</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Data/FruitData.js
@@ -0,0 +1,5 @@
+export const getFruitsData = () => [
+    { id: '1', name: 'Apple', description: 'A sweet red fruit.', image: '/images/apple.jpg' },
+    { id: '2', name: 'Banana', description: 'A long yellow fruit.', image: '/images/banana.jpg' },
+    { id: '3', name: 'Cherry', description: 'A small red fruit.', image: '/images/cherry.jpg' },
+  ];
